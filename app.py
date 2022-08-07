94e2572debf5b57d90eb07dd534996dcccf96642
Casey Davis <casey@cwdavis.net>
2022-08-07 15:21:20 -0400

    added Text FSM

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
 from netmiko import ConnectHandler
-import re,sys
+import re,sys,os
 import requests
 from flask import *
+templates = os.path.dirname(os.path.abspath(__file__)) + '/ntc-templates/ntc_templates/templates'
+os.environ['NET_TEXTFSM']= templates
 
 ########################
 app = Flask(__name__)
-
-app.route('/maclookup')
-def LOOKUPMAC():
-    mac = request.args.get('mac', None)
-    token=('01g9qj7ta0x1ws7gjr85024hz201g9qj8dnf27p8cx5f36d5cf1wy0ol7xs3gvvu')
-    response = requests.get(f'https://api.maclookup.app/v2/macs/{mac}/company/name?apiKey={token}')
-    return(response.text)
-
-
 
 @app.route('/show_ip_arp')
 def show_ip_arp():
@@ -22,7 +15,9 @@ def show_ip_arp():
     os = request.args.get('os', None)
     user = request.args.get('user', None)
     passwd = request.args.get('passwd', None)
-    return(f'This is a test for {IP} -- {os} -- {user} -- {passwd}')
+    net_connect=ConnectHandler(device_type='cisco_ios', ip=IP, username='autobot', password='WSX@#$rdx567-Prime')
+    showarp=net_connect.send_command("show ip arp",use_textfsm=True)
+    return(showarp)
     
 
 
