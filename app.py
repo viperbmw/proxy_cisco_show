00a0e3f145db5809d4bf7962c4cd2e7d175b065f
Casey Davis <casey@cwdavis.net>
2022-08-07 18:14:48 -0400

    Add Mac Addr

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from netmiko import ConnectHandler
 import re,sys,os
 import requests
 from flask import *
-templates = os.path.dirname(os.path.abspath(__file__)) + '/ntc-templates/ntc_templates/templates'
-os.environ['NET_TEXTFSM']= templates
 
 ########################
 app = Flask(__name__)
@@ -16,10 +14,18 @@ def show_ip_arp():
     user = request.args.get('user', 'autobot')
     passwd = request.args.get('passwd', 'WSX@#$rdx567-Prime')
     net_connect=ConnectHandler(device_type=os, ip=IP, username=user, password=passwd)
-    showarp=net_connect.send_command("show ip arp")#,use_textfsm=True)
+    showarp=net_connect.send_command("show ip arp",use_textfsm=True)
     return(showarp)
     
-
+@app.route('/show_mac_addr')
+def show_mac_addr():
+    IP = request.args.get('IP', None)
+    os = request.args.get('os', 'cisco_ios')
+    user = request.args.get('user', 'autobot')
+    passwd = request.args.get('passwd', 'WSX@#$rdx567-Prime')
+    net_connect=ConnectHandler(device_type=os, ip=IP, username=user, password=passwd)
+    showmac=net_connect.send_command("show mac address-table",use_textfsm=True)
+    return(showmac)
 
 
 if __name__ == '__main__':
