e293ce432fc4c665fa0e65188c97ee0d69a3b293
Casey Davis <casey@cwdavis.net>
2022-08-07 15:35:07 -0400

    setup

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from netmiko import ConnectHandler
 import re,sys,os
 import requests
 from flask import *
-templates = os.path.dirname(os.path.abspath(__file__)) + '/ntc-templates/ntc_templates/templates'
-os.environ['NET_TEXTFSM']= templates
+#templates = os.path.dirname(os.path.abspath(__file__)) + '/ntc-templates/ntc_templates/templates'
+#os.environ['NET_TEXTFSM']= templates
 
 ########################
 app = Flask(__name__)
@@ -12,11 +12,11 @@ app = Flask(__name__)
 @app.route('/show_ip_arp')
 def show_ip_arp():
     IP = request.args.get('IP', None)
-    os = request.args.get('os', None)
-    user = request.args.get('user', None)
-    passwd = request.args.get('passwd', None)
-    net_connect=ConnectHandler(device_type='cisco_ios', ip=IP, username='autobot', password='WSX@#$rdx567-Prime')
-    showarp=net_connect.send_command("show ip arp",use_textfsm=True)
+    os = request.args.get('os', 'cisco_ios')
+    user = request.args.get('user', 'autobot')
+    passwd = request.args.get('passwd', 'WSX@#$rdx567-Prime')
+    net_connect=ConnectHandler(device_type=os, ip=IP, username=user, password=passwd)
+    showarp=net_connect.send_command("show ip arp")#,use_textfsm=True)
     return(showarp)
     
 
